078953ea83d922e806e5d230231f4678f0b0fc36
JenHsuan <ofalpha@gmail.com>
2020-11-25 06:37:25 +0800

    add storage and context menus

--- background.js
+++ background.js
@@ -1,6 +1,43 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+function genericOnClick(info, tab) {
+    let data = "ID是：" + info.menuItemId + "\n" +  
+    "現在的網址是：" + info.pageUrl + "\n" +  
+    "選取的文字是：" + (info.selectionText ? info.selectionText : "") + "\n" +  
+    "現在hover元素的圖片來源：" + (info.srcUrl ? info.srcUrl : "") + "\n" +  
+    "現在hover的連結：" + (info.linkUrl ? info.linkUrl : "") + "\n" +  
+    "現在hover的frame是：" + (info.frameUrl ? info.frameUrl : "") + "\n";
 
-'use strict';
+    chrome.storage.sync.set({"color":data},function() {
+        //string or array of string or object keys
+        chrome.storage.sync.get("color",function(items) {
+            alert(items.color);
+        });
+    });
+};
+
+chrome.runtime.onInstalled.addListener(function() {
+    let parent = chrome.contextMenus.create({
+        "title": "Copy and format",
+        "contexts": ['all']    
+    });
+
+    let h1 = chrome.contextMenus.create({  
+        "title": "H1",  
+        "type": "normal",  
+        "contexts": ['all'],  
+        "parentId": parent,  
+        "onclick": genericOnClick  
+    });  
+
+    let h2 = chrome.contextMenus.create({  
+        "title": "H2",  
+        "type": "normal",  
+        "contexts": ['all'],  
+        "parentId": parent,  
+        "onclick": genericOnClick  
+    });  
+
+});
 
